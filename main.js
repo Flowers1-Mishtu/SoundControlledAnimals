e3cf4b2919ff0d3a4118ef20ba0a29bb16c01269
Flowers1-Mishtu <87982268+Flowers1-Mishtu@users.noreply.github.com>
2023-02-16 20:56:23 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-https://teachablemachine.withgoogle.com/models/chgXCOuaJ/
+
+function startclassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier =ml5.soundclassifier("https://teachablemachine.withgoogle.com/models/chgXCOuaJ/model.json", modelready)
+}
+
+function modelready()
+    {
+classifier.classify(gotresults);
+    }
